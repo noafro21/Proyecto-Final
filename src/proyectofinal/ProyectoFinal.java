98f057db174cd22bb3879e68ba35bcb27a91f050
Anoaf <anoaf@ucenfotec.ac.cr>
2024-11-13 21:16:34 -0600

    Se actualizan la Clase ProyectoFinal

--- src/proyectofinal/ProyectoFinal.java
+++ src/proyectofinal/ProyectoFinal.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package proyectofinal;
 
-/**
- *
- * @author adonf
- */
 public class ProyectoFinal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+ Usuario miSistema = new Usuario();
+       
+       miSistema.ingresarDatos();
+       miSistema.validarUsuario(); 
     }
-    
+  
 }
