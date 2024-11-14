006ce0e030870ff9c33a9cb5f18f03f03ae5d0f6
Anoaf <anoaf@ucenfotec.ac.cr>
2024-11-13 20:37:36 -0600

    Se agrega la clase MenuPrincipal

---
+++ src/proyectofinal/MenuPrincipal.java
@@ -0,0 +1,12 @@
+
+package proyectofinal;
+
+import javax.swing.JOptionPane;
+
+public class MenuPrincipal {
+    public void menuSistema()
+    {
+        JOptionPane.showMessageDialog(null, "Ingreso al menu correctamente");
+    }
+    
+}
